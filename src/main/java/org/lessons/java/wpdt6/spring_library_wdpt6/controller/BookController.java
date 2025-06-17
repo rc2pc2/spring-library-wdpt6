bf18fdacc4c8f8ee13e2c4946587a8fd5bf87d98
RC2PC2 <rcsquaredpcsquared@gmail.com>
2025-06-17 20:21:58 +0200

    added show and filtered index for Book resource

--- src/main/java/org/lessons/java/wpdt6/spring_library_wdpt6/controller/BookController.java
+++ src/main/java/org/lessons/java/wpdt6/spring_library_wdpt6/controller/BookController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.lessons.java.wpdt6.spring_library_wdpt6.model.Book;
 import org.lessons.java.wpdt6.spring_library_wdpt6.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -19,9 +22,23 @@ public class BookController {
     private BookRepository bookRepository;
 
     @GetMapping
-    public String index(Model model){
-        List<Book> books = bookRepository.findAll();
+    public String index(Model model, @RequestParam( name = "keyword", required = false) String keyword){
+
+        // & Se qualcuno ha cercato, allora cerchiamo
+        List<Book> books;
+        if ( keyword != null && !keyword.isEmpty()){
+            // % implementiamo la ricerca
+            books = bookRepository.findByTitleContainingIgnoreCase(keyword);
+        } else {
+            books = bookRepository.findAll(Sort.by("publicationDate"));
+        }
         model.addAttribute("books", books);
         return "books/index";
+    }
+
+    @GetMapping("/{id}") // $ app.com/books/2
+    public String show( @PathVariable("id") Integer id, Model model){
+        model.addAttribute("book", bookRepository.findById(id).get());
+        return "books/show";
     }
 }
--- src/main/java/org/lessons/java/wpdt6/spring_library_wdpt6/repository/BookRepository.java
+++ src/main/java/org/lessons/java/wpdt6/spring_library_wdpt6/repository/BookRepository.java
@@ -1,8 +1,15 @@
 package org.lessons.java.wpdt6.spring_library_wdpt6.repository;
 
+import java.util.List;
+
 import org.lessons.java.wpdt6.spring_library_wdpt6.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
     
+    public List<Book> findByTitleContainingIgnoreCase(String title);
+
+    public List<Book> findByTitle(String title);
+
+    public List<Book> findByAuthor(String author);
 }
